de9b9d8a5efd337652c076dbfe4221e57fc95efd
Kracav4ik <dikama2013@yandex.ru>
2017-03-15 00:21:58 +0300

    some texts in Russian

---
+++ input/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import re
+
+path = 'text.txt'
+
+print(path)
+
+with open(path, encoding='utf8') as inp:
+    text = re.sub(r'\W+', ' ', inp.read().lower().strip()).split()
+with open(path.replace('.txt', '-words.txt'), 'w', encoding='utf8') as out:
+    out.write(" ".join(text))
+
