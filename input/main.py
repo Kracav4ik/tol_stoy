6223c6cbddb5e329ea30d492b3ba4861dd966b5c
Kracav4ik <dikama2013@yandex.ru>
2017-03-15 23:55:38 +0300

    digits are not words

--- input/main.py
+++ input/main.py
@@ -7,7 +7,7 @@ path = 'text.txt'
 print(path)
 
 with open(path, encoding='utf8') as inp:
-    text = re.sub(r'\W+', ' ', inp.read().lower().strip()).split()
+    text = re.sub(r'[\W\d]+', ' ', inp.read().lower().strip()).split()
 with open(path.replace('.txt', '-words.txt'), 'w', encoding='utf8') as out:
     out.write(" ".join(text))
 
